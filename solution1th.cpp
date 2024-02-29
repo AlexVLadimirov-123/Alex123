d2fae592d616480e2b7f9185f2aafec4b55acedc
AlexVLadimirov-123 <160173508+AlexVLadimirov-123@users.noreply.github.com>
2024-02-29 20:49:43 +0300

    Update solution1th.cpp

--- solution1th.cpp
+++ solution1th.cpp
@@ -2,11 +2,11 @@ include <iostream>
 include <stdlib>
 int main()
 {
-    int massive [4]={};
-    for ( int i=1; i<4; i++)    
+int massive [4]={};
+for ( int i=1; i<4; i++)    
     {
 massive[i] = rand() % 40; 
 std::cout << massive [i]; 
-     }
+    }
 return 0;
  }

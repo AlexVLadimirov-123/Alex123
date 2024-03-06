cf4822022d363237eec918362ab0d5bea2e912d2
AlexVLadimirov-123 <160173508+AlexVLadimirov-123@users.noreply.github.com>
2024-03-06 22:59:46 +0300

    Create solution3th.cpp

---
+++ solution3th.cpp
@@ -0,0 +1,20 @@
+//Задача: Найти длину окружности, используя радиус//
+#include <iostream>
+using namespace std;
+
+int
+main () 
+ { 
+float a, b, c;
+  
+char o;
+  
+cout << "Enter the a,b,c";
+cin >> a >> o >> b >> o >> c;
+cout << a << endl, 
+cout << b << endl, 
+cout << c << endl;
+  
+return 0;
+
+}

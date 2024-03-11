bdbc6ec64c30aaecd399fd96d09c912873ffc73c
AlexVLadimirov-123 <160173508+AlexVLadimirov-123@users.noreply.github.com>
2024-03-11 13:26:24 +0300

    Create universityex.cpp

---
+++ universityex.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <math.h>
+  using namespace std;
+
+int
+main () 
+ { 
+int x, y;
+  
+float f;
+  
+float e = 2.7;
+  
+cout << " Enter the x, y";
+  
+cin >> x >> y;
+  
+if (x + y > 0, x > y ^ 2)
+	
+	{
+	  
+f = exp (x + y);
+	  
+cout << f;
+	
+}
+  
+  else
+  
+if 
+(x + y <= 0, x = y ^ 2)
+	
+	{
+	  
+f = exp (-x - y);
+	  
+cout << f;
+	
+}
+  
+  else
+	
+f = exp (x - y);
+  
+cout << f;
+  
+return 0;
+
+}

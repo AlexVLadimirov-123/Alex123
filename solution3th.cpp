3ae895711f010bcfefde0fcfd360bdb1ed908688
AlexVLadimirov-123 <160173508+AlexVLadimirov-123@users.noreply.github.com>
2024-03-13 11:09:02 +0300

    Update solution3th.cpp

--- solution3th.cpp
+++ solution3th.cpp
@@ -3,41 +3,36 @@
 #include <sstream>
 #include <iostream>
 using namespace std;
-
-int *
-gh (char a, char b, char c) 
- { 
+int*gh(char a, char b, char c)
+{
 int *arr = new int[3];
-  
 arr[0] = a;
-  
 arr[1] = b;
-  
 arr[2] = c;
-  
 for (int i = 0; i < 3; i++)
-	
-	{
-	  
+	{  
 cout << arr[i] << endl;
-	
 } 
 return arr;
-
 }
-
-
- 
 int 
 main () 
  { 
-char a, b, c;
-cout << "Enter the a,b,c";
-  
-scanf ("%d, %d, %d", &a, &b, &c);
-  
+     char a,b,c;
+     string str;
+     cout << "enter the a, b, c";
+     cin >> str;
+
+stringstream test("a, b, c");
+string segment;
+vector<string> seglist;
+
+while(getline(test, segment, ','))
+{
+   seglist.push_back(segment);
+}
+
 int *arr = gh (a, b, c);
-  
 return 0;
 
 }

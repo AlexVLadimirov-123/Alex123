d093fb9c19805916ff92ebc6494514b20a330104
AlexVLadimirov-123 <160173508+AlexVLadimirov-123@users.noreply.github.com>
2024-03-13 21:23:01 +0300

    Update solution3th.cpp

--- solution3th.cpp
+++ solution3th.cpp
@@ -4,8 +4,7 @@
 #include <vector>
 using namespace std;
 
-vector < int >
-StringOfnumbers (const string & input)
+vector < int > StringOfnumbers (const string & input)
 {
   
 vector < int > numbers;
